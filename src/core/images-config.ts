603ca4a005527336401dc2cc19c0d8b4f539be72
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2026-02-21 15:08:12 +0700

    add image

--- src/core/images-config.ts
+++ src/core/images-config.ts
@@ -1,5 +1,20 @@
 export const imageConfig = {
-    background: {},
+    background: {
+        earthglobeBg: "/background/earthglobe-bg.png",
+        globeBgDesktop: "/background/globe-bg-desktop.png",
+        desktop: {
+            bgBannerDesktop: "/background/desktop/bg-banner-desktop.png",
+            footerBgDesktop: "/background/desktop/footer-bg-desktop.png",
+        },
+        mobile: {
+            bgBannerMobile: "/background/mobile/bg-banner-mobile.png",
+            bgMobile: "/background/mobile/bg-mobile.png",
+            bgMobileSecondary: "/background/mobile/bg-secondary-mobile.png",
+            footerBgMobile: "/background/mobile/footer-bg-mobile.png",
+        },
+    },
     icon: {},
-    logo: {}
+    logo: {
+        stomatradeLogo: "/logo/stomatrade-logo.png",
+    },
 }
