a55170e33bd3a951b26edf65b38e873abac34010
ramadhanialfarizi <72063724+ramadhanialfarizi@users.noreply.github.com>
2026-02-22 13:37:34 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/features/landing-page/landing-page-view.tsx
@@ -0,0 +1,8 @@
+export default function LandingPageView() {
+    return (
+        <div>
+            <h1>Welcome to Stomatrade</h1>
+            {/* semua UI landing page di sini */}
+        </div>
+    );
+}
