279a9f211dd747d96613d3dfbb557309730ff298
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2026-03-14 05:56:50 +0700

    add navigation to dashboard

---
+++ src/features/collector-page/dashboard/dashboard-collector-view.tsx
@@ -0,0 +1,5 @@
+import { Typography } from "@mui/material";
+
+export default function DashboardCollectorView() {
+    return <Typography>Dashboard Collector</Typography>
+}
--- src/features/collector-page/login/hooks/use-login-collector.tsx
+++ src/features/collector-page/login/hooks/use-login-collector.tsx
@@ -1,12 +1,18 @@
 "use client";
 
+import { useNavigationUtils } from "@/core/hooks/use-navigation-utils";
+import { routes } from "@/core/routes";
 import { useCallback } from "react";
 
 
 export function useLoginPage() {
+    const { pushRoute } = useNavigationUtils();
+
     const handleConnectWallet = useCallback(() => {
         console.log("Continue with X clicked");
-    }, []);
+
+        pushRoute(routes.collector.dashboard)
+    }, [pushRoute]);
 
     const handleTermsClick = useCallback(() => {
         console.log("Terms clicked");
