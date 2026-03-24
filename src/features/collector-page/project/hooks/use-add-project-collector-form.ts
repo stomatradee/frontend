f06d92667208230c057ba3368c9ff26ce2a6eda8
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2026-03-24 16:32:53 +0700

    update hook

--- src/features/collector-page/project/hooks/use-add-project-collector-form.ts
+++ src/features/collector-page/project/hooks/use-add-project-collector-form.ts
@@ -1,12 +1,14 @@
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import z from "zod/v3";
 import { IDatePickerControl } from "@/core/types/common";
+import { ProfileRequestModel, ProfileResponseModel } from "@/repository/profile/model/profile-request-model";
+import { useAccount } from "wagmi";
+import { GetCollectorProfileRepository } from "@/repository/profile/profile-repository";
+import { toast } from "sonner";
 
 export default function useAddProjectCollectorForm() {
-    const [isLoading, setLoading] = useState(false);
-
     const RegisterCollectorSchema = z.object({
         contractAddress: z.string().min(1, "Contract Address is required"),
         role: z.string().min(1, "Role is required"),
@@ -49,6 +51,64 @@ export default function useAddProjectCollectorForm() {
             currentFundingPrice: "0",
         },
     });
+
+    const [isLoading, setLoading] = useState(false)
+    const { address } = useAccount();
+
+    const getUserProfile = useCallback(async () => {
+        setLoading(true)
+
+        try {
+            const data: ProfileRequestModel = {
+                contractAddress: address ?? "",
+                role: "collector",
+            };
+
+            const result = await GetCollectorProfileRepository(data)
+
+            if (result !== null) {
+                result.contractAddress = address ?? ""
+                methods.setValue("contractAddress", address ?? "")
+                methods.setValue("role", result.role)
+                methods.setValue("fullname", result.fullname)
+                methods.setValue("phoneNumber", result.phoneNumber)
+                methods.setValue("residenceId", result.residenceId)
+                methods.setValue("companyName", result.companyName)
+                methods.setValue("companyAddress", result.companyAddress)
+
+            } else {
+                toast.error("Data Not Found", {
+                    position: 'top-center',
+                    style: {
+                        width: '600px',
+                        left: '50%',
+                        right: '50%',
+                        transform: 'translate(-50%)',
+                        display: 'flex',
+                        alignItems: 'center',
+                    },
+                });
+            }
+        } catch (error) {
+            toast.error(`Get data failed: ${error}`, {
+                position: 'top-center',
+                style: {
+                    width: '600px',
+                    left: '50%',
+                    right: '50%',
+                    transform: 'translate(-50%)',
+                    display: 'flex',
+                    alignItems: 'center',
+                },
+            });
+        }
+
+        setLoading(false)
+    }, [address])
+
+    useEffect(() => {
+        getUserProfile()
+    }, [])
 
 
     return {
