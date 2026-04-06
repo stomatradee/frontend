ebe9405e6ea286299d31cfd5fff1c17370b4b2dd
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2026-04-06 22:47:35 +0700

    update

--- src/features/collector-page/project/hooks/use-my-project.ts
+++ src/features/collector-page/project/hooks/use-my-project.ts
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function useMyProject() {
     const [isLoading, setLoading] = useState(false)
+
+    const getMyProject = useCallback(async () => {
+        setLoading(true)
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+
+        setLoading(false)
+    }, [])
+
+    useEffect(() => {
+        getMyProject()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
     return {
         isLoading,
