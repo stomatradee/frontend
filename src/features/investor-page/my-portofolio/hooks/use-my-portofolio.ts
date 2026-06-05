d3db99dccf8ae93b38c5c38301bb630d36e08cd5
ramadhanialfarizi <ramadhani.alfarizi@gmail.com>
2026-06-05 21:37:58 +0700

    update my portofolio screen

--- src/features/investor-page/my-portofolio/hooks/use-my-portofolio.ts
+++ src/features/investor-page/my-portofolio/hooks/use-my-portofolio.ts
@@ -1,3 +1,54 @@
-export function useMyPortofolio() {
+import { routes } from "@/core/config/routes";
+import { useNavigationUtils } from "@/core/hooks/use-navigation-utils";
+import { OpenProjectResponseModel } from "@/repository/project/model/open-project-model";
+import { getOpenProject } from "@/repository/project/project-repository";
+import { useCallback, useEffect, useState } from "react";
+import { toast } from "sonner";
 
+export function useMyPortofolio() {
+    const [isLoading, setLoading] = useState(false)
+
+    const [data, setData] = useState<OpenProjectResponseModel | null>(null)
+
+    const { pushRoute } = useNavigationUtils();
+
+    const getProject = useCallback(async () => {
+        try {
+            setLoading(true)
+
+            const response = await getOpenProject()
+
+            setData(response)
+
+            setLoading(false)
+        } catch (error) {
+            setLoading(false)
+            toast.error(`Get data failed: ${error}`, {
+                position: 'top-center',
+                style: {
+                    width: '600px',
+                    left: '50%',
+                    right: '50%',
+                    transform: 'translate(-50%)',
+                    display: 'flex',
+                    alignItems: 'center',
+                },
+            });
+        }
+    }, [])
+
+    useEffect(() => {
+        getProject()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    const handleNavigateToProjectDetail = useCallback((projectId: string) => {
+        pushRoute(routes.investor.projectDetail, projectId);
+    }, [pushRoute])
+
+    return {
+        isLoading,
+        data,
+        handleNavigateToProjectDetail,
+    }
 }
--- src/features/investor-page/my-portofolio/my-portofolio-view.tsx
+++ src/features/investor-page/my-portofolio/my-portofolio-view.tsx
@@ -1,9 +1,74 @@
-import { Typography } from "@mui/material";
+"use client";
+
+import { themeConfig } from "@/core/config/theme-config";
+import { Box, Grid, Typography } from "@mui/material";
+import { useMyPortofolio } from "./hooks/use-my-portofolio";
+import { LoadingScreen } from "@/core/component/loading-component";
+import EmptyAssetComponent from "@/core/component/empty-asset-component";
+import { imageConfig } from "@/core/config/images-config";
+import ProjectCard from "@/core/component/project-card";
 
 export default function MyPortofolioView() {
+  const theme = themeConfig;
+
+  const { isLoading, data, handleNavigateToProjectDetail } = useMyPortofolio();
+
+  if (isLoading) {
+    return (
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+      >
+        <LoadingScreen />
+      </Box>
+    );
+  }
+
   return (
-    <Typography variant="h5" fontWeight={700}>
-      My Portofolio
-    </Typography>
+    <Box
+      display="flex"
+      flexDirection="column"
+      padding={{ xs: "100px 20px 40px", md: "20px" }}
+      margin="0 auto"
+    >
+      <Typography
+        variant="h1"
+        color={theme.colors.white}
+        fontWeight={600}
+        sx={{ fontSize: { xs: 16, sm: 18, md: 25 } }}
+      >
+        My Portofolio
+      </Typography>
+
+      {data?.projects.length === 0 ? (
+        <EmptyAssetComponent title="No Portofolio found Found" image={imageConfig.icon.confusedIconRich} />
+      ) : (
+        <Box paddingTop="32px">
+          <Grid container spacing={2}>
+            {data?.projects.map((project) => (
+              <ProjectCard
+                key={project.id}
+                assetName={project.metadata.assetName}
+                createdAt={project.createdAt}
+                fundingProgress={project.fundingProgress}
+                imageCID={project.metadata.imageCID}
+                maxFundingUSD={project.maxFundingUSD}
+                pricePerKg={project.pricePerKg}
+                returnRate={project.returnRate}
+                statusLabel={project.statusLabel}
+                totalFundedUSD={project.totalFundedUSD}
+                investorCount={project.investorCount}
+                id={project.id.toString()}
+                handleNavigateToProjectDetail={() => {
+                  handleNavigateToProjectDetail(project.id.toString())
+                }}
+              />
+            ))}
+          </Grid>
+        </Box>
+      )}
+    </Box>
   );
 }
